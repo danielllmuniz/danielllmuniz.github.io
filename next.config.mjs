f5be4420cb140d38163a021d1fd9567e30a42844
daniel muniz <munidaniel@gmail.com>
2024-07-31 23:55:21 -0300

    fixing images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  trailingSlash: true,
+  trailingSlash: isProd ? '/danielllmuniz.github.io/' : '',
   output: 'export',
   distDir: 'dist',
   images: {

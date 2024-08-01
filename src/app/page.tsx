5e626b084892cdb457385b217d865910975f3a0a
daniel muniz <munidaniel@gmail.com>
2024-08-01 18:38:01 -0300

    changing images to next/images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -394,17 +394,17 @@ It is also worth mentioning that its first application was at Heathrow Airport, 
     <p className="font-normal text-gray-500 text-xs md:text-base mb-10 md:mb-20">Below is a summary of the places I studied</p>
 
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <img src="image/brand-1.png" alt="Image"/>
+        <Image src="image/brand-1.png" alt="Image"/>
 
-        <img src="image/brand-2.png" alt="Image"/>
+        <Image src="image/brand-2.png" alt="Image"/>
 
-        <img src="image/brand-3.png" alt="Image"/>
+        <Image src="image/brand-3.png" alt="Image"/>
 
-        <img src="image/brand-4.png" alt="Image"/>
+        <Image src="image/brand-4.png" alt="Image"/>
 
-        <img src="image/brand-5.png" alt="Image"/>
+        <Image src="image/brand-5.png" alt="Image"/>
 
-        <img src="image/brand-6.png" alt="Image"/>
+        <Image src="image/brand-6.png" alt="Image"/>
     </div>
 
 </div>

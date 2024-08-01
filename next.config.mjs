c516581cf56d45bf1fc2117a02e987039b3b157e
daniel muniz <munidaniel@gmail.com>
2024-07-31 23:43:03 -0300

    fixing styles issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/danielllmuniz.github.io',
   trailingSlash: true,
   output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;

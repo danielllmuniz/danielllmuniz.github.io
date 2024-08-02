2bbfbd83abbe4c4ea631b20bb6fb59e8b2f0072e
daniel muniz <munidaniel@gmail.com>
2024-08-01 21:37:46 -0300

    fixing tipography error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -234,9 +234,7 @@ export default function Home() {
                 <div>
                     <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Router Management</h1>
 
-                    <p className="font-normal text-gray-500 text-sm md:text-base">The project was a sophisticated router management solution provided as a SaaS to telecom companies, leveraging a multitenant architecture for scalability and efficiency. As the backend lead, I developed an API provider in Node.js using Express, Sequelize, MySQL, and MongoDB to optimize performance for specific queries. The backend architecture included several microservices developed in Golang, one of which was a domain filter that utilized a binary access control system. To handle the high volume of requests, we integrated Redis for caching to enhance performance.
-
-The system also employed MQTT for real-time communication with routers, enabling dynamic message exchanges. Additionally, I supported the DevOps team, managing environments initially hosted on Digital Ocean with Docker and Kubernetes and played a pivotal role in migrating the infrastructure to Google Cloud's GKE, ensuring a seamless transition and robust deployment pipeline.</p>
+                    <p className="font-normal text-gray-500 text-sm md:text-base">{"The project was a sophisticated router management solution provided as a SaaS to telecom companies, leveraging a multitenant architecture for scalability and efficiency. As the backend lead, I developed an API provider in Node.js using Express, Sequelize, MySQL, and MongoDB to optimize performance for specific queries. The backend architecture included several microservices developed in Golang, one of which was a domain filter that utilized a binary access control system. To handle the high volume of requests, we integrated Redis for caching to enhance performance. The system also employed MQTT for real-time communication with routers, enabling dynamic message exchanges. Additionally, I supported the DevOps team, managing environments initially hosted on Digital Ocean with Docker and Kubernetes and played a pivotal role in migrating the infrastructure to Google Cloud's GKE, ensuring a seamless transition and robust deployment pipeline."}</p>
                 </div>
             </div>
             
@@ -248,9 +246,7 @@ The system also employed MQTT for real-time communication with routers, enabling
                 <div>
                     <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Solar Energy Proposal Generator</h1>
 
-                    <p className="font-normal text-gray-500 text-sm md:text-base">This project was a proposal generator for a solar energy enterprise, designed to assist sellers in calculating the optimal solar energy kit for customers. The software factored in the city's solar incidence, roof specifications, energy voltage, and average monthly energy consumption to recommend the best kit, providing a price estimate and generating a PDF proposal for sellers to present to customers.
-
-Initially, the platform was a single-tenant SaaS application developed using Laravel, with the full stack hosted on Digital Ocean. However, as demand grew and the number of concurrent users increased, we developed a second version of the platform using Node.js. This new version leveraged AWS Lambda functions to effectively manage the inconsistent demand, providing a scalable and responsive solution for the enterprise.</p>
+                    <p className="font-normal text-gray-500 text-sm md:text-base">{"This project was a proposal generator for a solar energy enterprise, designed to assist sellers in calculating the optimal solar energy kit for customers. The software factored in the city's solar incidence, roof specifications, energy voltage, and average monthly energy consumption to recommend the best kit, providing a price estimate and generating a PDF proposal for sellers to present to customers. Initially, the platform was a single-tenant SaaS application developed using Laravel, with the full stack hosted on Digital Ocean. However, as demand grew and the number of concurrent users increased, we developed a second version of the platform using Node.js. This new version leveraged AWS Lambda functions to effectively manage the inconsistent demand, providing a scalable and responsive solution for the enterprise."}</p>
                 </div>
             </div>
             
@@ -262,13 +258,7 @@ Initially, the platform was a single-tenant SaaS application developed using Lar
                 <div>
                     <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Beacon Analytics</h1>
 
-                    <p className="font-normal text-gray-500 text-sm md:text-base">The project was a map analytics application designed to track the real-time locations of airport employees using beacon technology, first implemented at Heathrow Airport. As the frontend developer, I was responsible for creating the comprehensive dashboard that displayed this data, ensuring the interface was intuitive and user-friendly.
-
-Given the application's security-critical nature, it required managing large streams of data with real-time updates to maintain situational awareness and enhance airport security operations. I implemented robust security features, including two-factor authentication and email-based authentication, to ensure secure access to the system.
-
-The application was designed to be responsive, ensuring optimal performance and usability across all types of devices, from desktops to mobile phones. This project demonstrated my ability to handle real-time data streams, implement advanced security measures, and create responsive, user-focused interfaces.
-
-It is also worth mentioning that its first application was at Heathrow Airport, being used in other airports after that.</p>
+                    <p className="font-normal text-gray-500 text-sm md:text-base">{"The project was a map analytics application designed to track the real-time locations of airport employees using beacon technology, first implemented at Heathrow Airport. As the frontend developer, I was responsible for creating the comprehensive dashboard that displayed this data, ensuring the interface was intuitive and user-friendly. Given the application's security-critical nature, it required managing large streams of data with real-time updates to maintain situational awareness and enhance airport security operations. I implemented robust security features, including two-factor authentication and email-based authentication, to ensure secure access to the system. The application was designed to be responsive, ensuring optimal performance and usability across all types of devices, from desktops to mobile phones. This project demonstrated my ability to handle real-time data streams, implement advanced security measures, and create responsive, user-focused interfaces. It is also worth mentioning that its first application was at Heathrow Airport, being used in other airports after that."}</p>
                 </div>
             </div>
         </div>
